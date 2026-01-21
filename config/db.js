d8844bafabfc276d219c81fbdfdf46ea4eae6db8
ayeshae-111 <ayeshaelahi111@gmail.com>
2026-01-21 05:50:55 -0800

    Fix Mongo DB URI for Render

--- config/db.js
+++ config/db.js
@@ -2,10 +2,14 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI is missing");
+    }
+
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected");
-  } catch (err) {
-    console.error("DB connection failed:", err.message);
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error("DB connection failed:", error.message);
     process.exit(1);
   }
 };
